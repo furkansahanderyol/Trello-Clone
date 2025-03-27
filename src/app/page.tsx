429f185dcbb1cc601a0dd5a379f9027edd815cc0
Furkan <FSDeryol@gmail.com>
2025-03-27 16:54:26 +0300

    Clsx library added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import styles from "./page.module.scss"
 
 export default function Home() {
   return (
-    <main className={styles.container}>
+    <main>
       <div>Hello World!</div>
     </main>
   )
